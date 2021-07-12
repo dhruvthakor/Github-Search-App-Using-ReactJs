dac510c18ba354a71b1fde42e8de7130a52821f5
Dhruv Thakor <dhruvthakor234@gmail.com>
2021-07-12 21:39:00 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  
+  apiKey: "AIzaSyDATFrtdI4CfMHknlAJaLkuSnY291w-VpQ",
+  authDomain: "mygitapp-13998.firebaseapp.com",
+  projectId: "mygitapp-13998",
+  storageBucket: "mygitapp-13998.appspot.com",
+  messagingSenderId: "1033167139996",
+  appId: "1:1033167139996:web:3acf9395652df579666959"
+ 
+};
---
+++ src/components/Repos.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+const Repos = ({ repos_url }) => {
+  const [repos, setRepos] = useState([]);
+
+  const fetchRepos = async () => {
+    const { data } = await Axios.get(repos_url);
+    setRepos(data);
+  };
+
+  useEffect(() => {
+    fetchRepos();
+  }, [repos_url]);
+
+  return (
+    <ListGroup>
+      {repos.map(repo => (
+        <ListGroupItem key={repo.id}>
+          <div className="text-primary h5">{repo.name}</div>
+          <div className="text-secondary">{repo.language}</div>
+          <div className="text-dark">{repo.description}</div>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+};
+
+export default Repos;
